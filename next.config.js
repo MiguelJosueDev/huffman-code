3f55d9ad1a41bae66b80d6e73fd68b29ddba8f29
Miguel Cruz <miguel.cruz@lifebeacon.com>
2024-12-05 09:17:42 -0600

    sadsad
    as

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    "extends": [
+        "eslint:recommended",
+        "next/core-web-vitals"
+    ],
+    "rules": {
+    }
 }
 
 module.exports = nextConfig;
